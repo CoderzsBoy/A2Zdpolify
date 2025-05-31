ee4b0f357c43ae17c901940cf4b96b096b31cc61
CoderzsBoy <newghanshyamversion57@gmail.com>
2025-05-31 09:05:20 +0530

    i3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,24 @@ export default function RootLayout({
           </main>
           <Footer />
         </AppProviders>
+        {/*Start of Tawk.to Script*/}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/683a7823646464190ff81eb1/1isi6ajhl';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
+        {/*End of Tawk.to Script*/}
       </body>
     </html>
   );
